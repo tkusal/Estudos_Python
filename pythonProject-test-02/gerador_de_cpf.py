686866ebe1d097559917cd902590088ce0a39772
tkusal <thiago.kusal@outlook.com>
2022-09-01 00:09:03 -0300

    up

--- pythonProject-test-02/gerador_de_cpf.py
+++ pythonProject-test-02/gerador_de_cpf.py
@@ -1,15 +1,16 @@
+from random import randint
+
 loop = 'S'
 soma = 0
 ref = 10
 
 while loop == 'S':
     while True:
-        cpf = input('Informe um CPF(apenas números): ')
-        if len(cpf) != 11 or cpf == cpf[::-1]:
-            print('Número de CPF Inválido\n\n')
+        cpf = str(randint(100000000, 999999999))
+        if cpf == cpf[::-1]:
+            continue
         else:
-            print('Validando CPF...\n\n')
-            verif_cpf = cpf[:-2]
+            verif_cpf = cpf
             break
 
     for i in range(19):
@@ -27,13 +28,14 @@ while loop == 'S':
                 digit = 0
             soma = 0
             verif_cpf += str(digit)
+        
+    cpf = '{}.{}.{}-{}'.format(verif_cpf[:3], verif_cpf[3:6], verif_cpf[6:9], verif_cpf[9:])
 
-    msg = ('CPF Válido' if cpf == verif_cpf else 'CPF Inválido')
-    print(f'{msg}\n')
+    print(f'O novo CPF é: {cpf}\n')
 
     looping = True
     while looping:
-        loop = input('Deseja testar outro CPF? (S/ N)_ ')
+        loop = input('Deseja gerar outro CPF? (S/ N)_ ')
         loop = loop.upper()
         looping = False if loop == 'S' or loop == 'N' else True
         if not loop == 'S' or loop == 'N':
